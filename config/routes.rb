dc526a7a2a35d54425de9920e6cb140ead5b863b
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-03-31 03:31:55 +0000

    [Add]管理者トップページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    root to: "homes#top"
+  end
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
 end
