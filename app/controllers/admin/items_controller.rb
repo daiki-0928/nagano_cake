a9b55e489fe140c3973e25a74700a7dcbf4c3dc8
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-04-03 09:06:35 +0000

    [Add]ジャンル画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,13 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def new
    @item = Item.new
   end
-  
+
   def create
    @item = Item.new(item_params)
     if @item.save
-     　redirect_to admin_item_path
+       redirect_to admin_item_path(@item)
     else
        render :new
     end
@@ -24,8 +24,19 @@ class Admin::ItemsController < ApplicationController
   def edit
    @item = Item.find(params[:id])
   end
-  
+
   def update
    @item = Item.find(params[:id])
+    if @item.update(item_params)
+     redirect_to admin_item_path(@item)
+    else
+     render :edit
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price)
   end
 end
