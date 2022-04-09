2ebeb4fda9cd69f95f62eb52ac54fa8862634716
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-04-09 08:58:48 +0000

    [Add]登録情報編集画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,13 +1,24 @@
 class Public::CustomersController < ApplicationController
   def show
-    @customer = Customer.find(params[:id])
+    @customer = current_customer
   end
 
   def edit
-    @customer = Customer.find(params[:id])
+    @customer = current_customer
   end
 
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+     redirect_to customers_my_page_path(@customer.id)
+    else
+     render :edit
+    end
+  end
+
+  private
+
   def customer_params
-    params.require(:customer).permit(:last_name, :last_name_kana, :first_name, :first_name_kana)
+    params.require(:customer).permit(:last_name, :last_name_kana, :first_name, :first_name_kana, :address, :email, :postal_code, :telephone_number)
   end
 end
