597ff8f9fe1791413c481beea5b27c8185cd6402
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-10-30 14:00:27 +0000

    [Add]カスタマーモデl編集

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,6 +8,7 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
 
+  
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
