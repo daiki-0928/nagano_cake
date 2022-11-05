f6c8f27beb09006ab79de9d27e47ef26cfca14dc
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-11-05 10:17:24 +0000

    [Add]インデント修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,10 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
   has_many :addresses, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
@@ -18,4 +18,5 @@ class Customer < ApplicationRecord
   validates :postal_code, presence: true
   validates :address, presence: true
   validates :telephone_number, presence: true
+
 end
