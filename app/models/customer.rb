200f26f24e590f214e86aa7658c8c2e9fa2b7fe6
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-12-10 14:53:53 +0000

    [Add]調整

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,6 +9,7 @@ class Customer < ApplicationRecord
   has_many :orders, dependent: :destroy
 
   validates :last_name, presence: true
+  
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
